967d732cce9f8804c71c32c42d9247b170fd87b9
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-01-20 10:23:21 +0100

    dodanie maina

--- src/main/java/struktury_danych/Tablice.java
+++ src/main/java/struktury_danych/Tablice.java
@@ -1,6 +1,6 @@
 package struktury_danych;
 
-public class Zadanie1 {
+public class Tablice {
 
 //Dana jest tablica liczb calkowitych. Wypisz:
 //i wszystkie liczby od końca będące na parzystych indeksach
@@ -8,7 +8,7 @@ public class Zadanie1 {
 // wynik sumy 5 poczatkowych liczb i odejmując ostatni element, zakłądając, że tablica jest rozmiaru co najmniej 6
 
 
-    static void evenReversed (int tab[]) {
+    public static void showEvenReversed(int tab[]) {
         for (int i = tab.length-1; i >= 0; i--) {
             if (i%2 == 0) {
                 System.out.println(tab[i]);
@@ -16,7 +16,7 @@ public class Zadanie1 {
         }
     }
 
-    static int sumDividedBy3 (int tab[]) {
+    public static int sumDividedBy3 (int tab[]) {
         int sum = 0;
         for (int i = 0; i < tab.length; i++) {
             if (tab[i]%3 == 0) {
@@ -26,7 +26,7 @@ public class Zadanie1 {
         return sum;
     }
 
-    static int sumOfFirst5 (int tab[]) {
+    public static int sumOfFirst5 (int tab[]) {
         int sum = 0;
         if (tab.length < 6) throw new IllegalArgumentException("Tablica za malego rozmiaru!");
         else {
@@ -36,19 +36,4 @@ public class Zadanie1 {
         }
         return sum;
     }
-
-    public static void main(String[] args) {
-
-        int[] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] tab2 = {11, 122, 1231, 31231, 23123, 1, 23, 123, 1321};
-        int[] tab3 = {2, 1, 2, 3, 4};
-        System.out.println("Metoda 1:");
-        evenReversed(tab);
-        System.out.println("Metoda 2:");
-        System.out.println(sumDividedBy3(tab));
-        System.out.println("Metoda 3:");
-        System.out.println(sumOfFirst5(tab));
-
-    }
-
 }
