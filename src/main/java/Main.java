a9a8cb3b7382d520b063f744f7e825da7d5a218b
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-01-26 13:20:18 +0100

    dodane głupoty w ramach testu2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -201,6 +201,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nic");
+        }
 
 //        wywolajTablice();
 //        wywolajStosTab();
